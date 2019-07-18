c1836123d1122ce23077b7535cfdcc435db6b68b
alyssadm <ahlisa.dm@gmail.com>
2019-07-18 12:57:01 -0400

    added basic html for front page and started basic JS

--- blind_book_date/app.js
+++ blind_book_date/app.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+$( () => {
+  // ================
+  // GRABBING ELEMENTS
+  // ================
+
+  $('select.genre').change( () => {
+    let selectedGenre = $(event.currentTarget).children('option:selected').val();
+    console.log(selectedGenre);
+  })
+
+  })
+
+
+
+  $.getJSON('https://www.googleapis.com/books/v1/volumes?q=subject:thriller&startIndex=100&maxResults=40&key=AIzaSyCd0ecTLD6dtmD-DfQCX3bd_dtn-siboXc', (data) => {
+    // console.log(data);
+  })
