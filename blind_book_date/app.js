ef4f636e048e283e25a0a99249492d6511a6c257
alyssadm <ahlisa.dm@gmail.com>
2019-07-18 13:38:04 -0400

    input fields on the front page now interact with the API

--- blind_book_date/app.js
+++ blind_book_date/app.js
@@ -1,17 +1,63 @@
 $( () => {
-  // ================
-  // GRABBING ELEMENTS
-  // ================
 
-  $('select.genre').change( () => {
-    let selectedGenre = $(event.currentTarget).children('option:selected').val();
-    console.log(selectedGenre);
-  })
+  // =========================================================
+  //                   USER INPUT OBJECT
+  // =========================================================
+  const userInput = {
+    genre: $('#genre').children('option:selected').val(),
+    bookAmt: 5,
+  };
 
-  })
+  // =========================================================
+  //                       APP LOGIC
+  // =========================================================
+  const app = {
+    clearMain: () => {
+      $('main').empty();
+    }
+    // launchTinder: (data) => {
+    //   data.
+    // }
+  };
 
 
+  // =========================================================
+  //                    EVENT HANDLERS
+  // =========================================================
+  const eventHandlers = {
 
-  $.getJSON('https://www.googleapis.com/books/v1/volumes?q=subject:thriller&startIndex=100&maxResults=40&key=AIzaSyCd0ecTLD6dtmD-DfQCX3bd_dtn-siboXc', (data) => {
-    // console.log(data);
-  })
+  // ===============
+  // updateVal()
+  // Runs every time the genre or amount of books changes
+  // Updates the value of each variable in the user object
+  // ===============
+    updateVal: () => {
+      let $id = $(event.currentTarget).attr('id');
+      userInput[$id] = $(event.currentTarget).val();
+    },
+
+  // ===============
+  // prepareGame()
+  // Runs upon click of "go" button
+  // Gets a random selection of books from the desired genre and prepares the next page
+  // ===============
+    prepareGame: () => {
+      $.getJSON(`https://www.googleapis.com/books/v1/volumes?q=subject:${userInput.genre}&startIndex=100&maxResults=40&key=AIzaSyCd0ecTLD6dtmD-DfQCX3bd_dtn-siboXc`, (data) => {
+        let minIndex = data.totalItems - 40;
+        let randomIndex = Math.floor(Math.random() * minIndex);
+        $.getJSON(`https://www.googleapis.com/books/v1/volumes?q=subject:${userInput.genre}&startIndex=${randomIndex}&maxResults=40&key=AIzaSyCd0ecTLD6dtmD-DfQCX3bd_dtn-siboXc`, (data) => {
+          app.clearMain();
+          // app.launchTinder(data);
+        })
+      })
+    }
+  };
+
+  // =========================================================
+  //                    EVENT LISTENERS
+  // =========================================================
+  $('#bookAmt').change(eventHandlers.updateVal);
+  $('#genre').change(eventHandlers.updateVal);
+  $('.go-div button').on('click', eventHandlers.prepareGame);
+
+});
