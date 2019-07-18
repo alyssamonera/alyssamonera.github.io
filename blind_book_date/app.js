ab8df2c7ce0baafd04a84540c5055c2925c07728
alyssadm <ahlisa.dm@gmail.com>
2019-07-18 15:47:03 -0400

    app now prints a random summary to the DOM

--- blind_book_date/app.js
+++ blind_book_date/app.js
@@ -12,12 +12,75 @@ $( () => {
   //                       APP LOGIC
   // =========================================================
   const app = {
-    clearMain: () => {
-      $('main').empty();
+    bookArray: [],
+    summaryArray: [],
+    titleArray: [],
+    coverArray: [],
+    currentSummary: "",
+
+    // ===============
+    // runGame(index)
+    // Runs inside of eventHandlers.prepareGame()
+    // Runs user input as parameters in the API, then filters the data through app.populateArrays()
+    // ===============
+      runGame: (index) => {
+        $.getJSON(`https://www.googleapis.com/books/v1/volumes?q=subject:${userInput.genre}&startIndex=${index}&maxResults=${userInput.bookAmt + 10}&langRestrict=en&key=AIzaSyCd0ecTLD6dtmD-DfQCX3bd_dtn-siboXc`, (data) => {
+          app.clearMain();
+          app.populateArrays(data);
+          app.printDOM();
+        })
+      },
+
+    // ===============
+    // clearMain()
+    // Runs inside of this.runGame()
+    // Clears the main div to make room for the next stage of the game
+    // ===============
+      clearMain: () => {
+        $('main').empty();
+      },
+
+    // ===============
+    // populateArrays(data)
+    // Runs inside of this.runGame()
+    // Stores the summaries, titles, authors, and cover image URLs into appropriate arrays
+    // ===============
+    populateArrays: (data) => {
+      this.bookArray = data.items;
+      let i = 0;
+      while (i < userInput.bookAmt){
+        if (this.bookArray[i].volumeInfo.description.length > 400){
+          if (this.bookArray[i].volumeInfo.imageLinks){
+            let summary = this.bookArray[i].volumeInfo.description;
+            let title = this.bookArray[i].volumeInfo.title;
+            let cover = this.bookArray[i].volumeInfo.imageLinks.thumbnail;
+            app.summaryArray.push(summary);
+            app.titleArray.push(title);
+            app.coverArray.push(cover);
+          }
+        }
+        i++
+      }
+    },
+
+    printDOM: () => {
+      app.currentSummary = app.summaryArray[0];
+      let innerhtml =
+        `<div class=tinder-container>
+          <div class=swipe-container>
+            <button></button>
+            <p>Swipe Left</p>
+          </div>
+          <div class=book-container>
+            <p>${app.currentSummary}</p>
+          </div>
+          <div class=swipe-container>
+            <button></button>
+            <p>Swipe Right</p>
+          </div>`;
+      $('main').append(innerhtml);
+      $('div.swipe-container > button').on('click', console.log("this works"));
     }
-    // launchTinder: (data) => {
-    //   data.
-    // }
   };
 
 
@@ -39,17 +102,13 @@ $( () => {
   // ===============
   // prepareGame()
   // Runs upon click of "go" button
-  // Gets a random selection of books from the desired genre and prepares the next page
+  // Picks a random index num based on the length of the data's book array, then runs it through app.runGame()
   // ===============
     prepareGame: () => {
-      $.getJSON(`https://www.googleapis.com/books/v1/volumes?q=subject:${userInput.genre}&startIndex=100&maxResults=40&key=AIzaSyCd0ecTLD6dtmD-DfQCX3bd_dtn-siboXc`, (data) => {
+      $.getJSON(`https://www.googleapis.com/books/v1/volumes?q=subject:${userInput.genre}&langRestrict=en&key=AIzaSyCd0ecTLD6dtmD-DfQCX3bd_dtn-siboXc`, (data) => {
         let minIndex = data.totalItems - 40;
         let randomIndex = Math.floor(Math.random() * minIndex);
-        $.getJSON(`https://www.googleapis.com/books/v1/volumes?q=subject:${userInput.genre}&startIndex=${randomIndex}&maxResults=40&key=AIzaSyCd0ecTLD6dtmD-DfQCX3bd_dtn-siboXc`, (data) => {
-          app.clearMain();
-          // app.launchTinder(data);
-        })
-      })
+        app.runGame(randomIndex)})
     }
   };
 
