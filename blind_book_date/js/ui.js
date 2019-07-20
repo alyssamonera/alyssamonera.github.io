287450d7c8f0a1e6f35fd305d1d44dbc1d13a8df
alyssadm <ahlisa.dm@gmail.com>
2019-07-20 10:24:28 -0400

    right-swipes can now be stored in local storage

--- blind_book_date/js/ui.js
+++ blind_book_date/js/ui.js
@@ -12,6 +12,8 @@ const userInput = {
   key: undefined,
 };
 
+
+
 // ====================
 // userLibrary (object)
 // Tracks/stores the user's selected books + book data from the API
@@ -21,6 +23,13 @@ const userLibrary = {
   datesArray: [],
   currentIndex: 0,
   likedBooks: 0,
+
+  // =================
+  // addNewDate()
+  // Runs on right swipe
+  // Creates and stores a right-swiped book object
+  // JSON/stringify solution found at: https://stackoverflow.com/questions/16083919/push-json-objects-to-array-in-localstorage
+  // =================
   addNewDate: () => {
     let i = userLibrary.currentIndex;
     let newDate = userLibrary.bookArray[i];
@@ -40,12 +49,25 @@ const userLibrary = {
         </ul>
       </div>`;
     userLibrary.datesArray.push(newDate);
+    let storedBooks = JSON.parse(localStorage.getItem("books"));
+    storedBooks.push(newDate);
+    localStorage.setItem("books", JSON.stringify(storedBooks))
+    console.log(storedBooks);
   },
+
+  // =================
+  // getSavedBooks()
+  // Runs on launch
+  // Checks if there is anything in local storage
+  // =================
   getSavedBooks: () => {
-    if (!localStorage.getItem("savedBooks")){
-      console.log("there are no saved books yet");
+    if (localStorage.length != 0){
+      let storedBooks = JSON.parse(localStorage.getItem("books"));
+      console.log(storedBooks);
     } else {
-      console.log("there are some books saved here");
+      let books = [];
+      localStorage.setItem("books", JSON.stringify(books));
     }
   }
-}
+
+};
